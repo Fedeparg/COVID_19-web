cf18a80b15a0234f91272d8297ab5fa6e0942832
Federico Pardo <fpg23feb98@gmail.com>
2021-03-26 15:22:34 +0100

    Estructura de la web generada

--- web.py
+++ web.py
@@ -17,18 +17,127 @@ app.layout = html.Div(children=[
     html.Div(children='Desarrollado por la Universidad de Murcia',
              style={'textAlign': 'center'}),
 
+    html.Br(),
+
     html.Div([
 
         # Columna Izquierda
         html.Div(
-            # Seleccion de modelo
+
             [
+                # Seleccion de modelo
                 'Selecciona un modelo:',
                 dcc.Dropdown(
                     id='operation',
                     options=[{'label': i, 'value': i} for i in operaciones],
                     value=''
                 ),
+                html.Br(),
+
+                # Ingreso
+                'Ingreso:', dcc.RadioItems(
+                    id='ingreso',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Si', 'No']],
+                    value='No',
+                    labelStyle={'display': 'inline-block'}
+                ),
+                html.Br(),
+
+                # NPC (Numero Patologias Cronicas)
+                'NPC (Numero Patologias Cronicas):', dcc.Input(
+                    id='npc',
+                    value='0',
+                    type='number'
+                ),
+                html.Br(),
+                html.Br(),
+
+                # Estrato
+                'Estrato:', dcc.Input(
+                    id='estrato',
+                    value='0',
+                    type='number'
+                ),
+
+                html.Br(),
+                html.Br(),
+
+                # HTA
+                'HTA', dcc.RadioItems(
+                    id='hta',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # DEP
+                'DEP', dcc.RadioItems(
+                    id='dep',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # VIH
+                'VIH', dcc.RadioItems(
+                    id='vih',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # IRC
+                'IRC', dcc.RadioItems(
+                    id='irc',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # OST
+                'OST', dcc.RadioItems(
+                    id='ost',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # Artritis
+                'Artritis', dcc.RadioItems(
+                    id='Artritis',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # DC
+                'DC', dcc.RadioItems(
+                    id='dc',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
 
             ],
             style={'width': '50%', 'display': 'inline-block', 'marginLeft': '10%', 'align': 'center'}),
@@ -48,8 +157,107 @@ app.layout = html.Div(children=[
                 ),
                 html.Br(),
 
+                # Edad
                 'Edad', dcc.Input(id='edad', value='', type='number'),
 
+                html.Br(),
+                html.Br(),
+
+                # NSIST
+                'NSIST:', dcc.Input(
+                    id='nsist',
+                    value='0',
+                    type='number'
+                ),
+
+                html.Br(),
+                html.Br(),
+
+                # DM
+                'DM', dcc.RadioItems(
+                    id='dm',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # IC
+                'IC', dcc.RadioItems(
+                    id='ic',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # EPOC
+                'EPOC', dcc.RadioItems(
+                    id='epoc',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # CI
+                'CI', dcc.RadioItems(
+                    id='ci',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # ACV
+                'ACV', dcc.RadioItems(
+                    id='acv',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # CIR
+                'CIR', dcc.RadioItems(
+                    id='cir',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # Artrosis
+                'Artrosis', dcc.RadioItems(
+                    id='artrosis',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
+
+                html.Br(),
+
+                # DEM
+                'DEM', dcc.RadioItems(
+                    id='dem',
+                    options=[{'label': i, 'value': i}
+                             for i in ['Verdadero', 'Falso']],
+                    value='Falso',
+                    labelStyle={'display': 'inline-block'}
+                ),
 
 
             ],
